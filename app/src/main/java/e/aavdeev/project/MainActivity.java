4f8d3fddf64a30a77c9e29a35a4a092fc0005e8a
Avdey <avdey87@gmail.com>
2018-09-18 16:01:08 +0300

    Initial commit

---
+++ app/src/main/java/e/aavdeev/project/MainActivity.java
@@ -0,0 +1,52 @@
+package e.aavdeev.project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private Button startButton;
+    private Button returnButton;
+    private Button stopButton;
+    private int status = 0;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressbar);
+        startButton = findViewById(R.id.start_button);
+        returnButton = findViewById(R.id.return_button);
+        stopButton = findViewById(R.id.stop_button);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MyService.class);
+                startService(intent);
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MyService.class);
+                stopService(intent);
+            }
+        });
+
+    }
+
+
+
+
+}
